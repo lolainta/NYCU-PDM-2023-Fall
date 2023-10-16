4c02fd9d35adfb376df37ccb39c59060d0e2685d
LolaInTa Chi <lolainta@gmail.com>
2023-10-16 18:16:57 +0800

    style: reformat template

--- hw1/load.py
+++ hw1/load.py
@@ -35,9 +35,11 @@ cam_extr = []
 def transform_rgb_bgr(image):
     return image[:, :, [2, 1, 0]]
 
+
 def transform_depth(image):
     depth_img = (image / 10 * 255).astype(np.uint8)
     return depth_img
+
 
 def transform_semantic(semantic_obs):
     semantic_img = Image.new("P", (semantic_obs.shape[1], semantic_obs.shape[0]))
@@ -46,6 +48,7 @@ def transform_semantic(semantic_obs):
     semantic_img = semantic_img.convert("RGB")
     semantic_img = cv2.cvtColor(np.asarray(semantic_img), cv2.COLOR_RGB2BGR)
     return semantic_img
+
 
 def make_simple_cfg(settings):
     # simulator backend
@@ -68,7 +71,7 @@ def make_simple_cfg(settings):
     ]
     rgb_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
 
-    #depth snesor
+    # depth snesor
     depth_sensor_spec = habitat_sim.CameraSensorSpec()
     depth_sensor_spec.uuid = "depth_sensor"
     depth_sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
@@ -81,7 +84,7 @@ def make_simple_cfg(settings):
     ]
     depth_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
 
-    #semantic snesor
+    # semantic snesor
     semantic_sensor_spec = habitat_sim.CameraSensorSpec()
     semantic_sensor_spec.uuid = "semantic_sensor"
     semantic_sensor_spec.sensor_type = habitat_sim.SensorType.SEMANTIC
@@ -94,35 +97,64 @@ def make_simple_cfg(settings):
     ]
     semantic_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
 
-    agent_cfg.sensor_specifications = [rgb_sensor_spec, depth_sensor_spec, semantic_sensor_spec]
+    agent_cfg.sensor_specifications = [
+        rgb_sensor_spec,
+        depth_sensor_spec,
+        semantic_sensor_spec,
+    ]
 
     return habitat_sim.Configuration(sim_cfg, [agent_cfg])
 
-def navigateAndSee(action="", data_root='data_collection/second_floor/'):
+
+def navigateAndSee(action="", data_root="data_collection/second_floor/"):
     global count
     observations = sim.step(action)
-    #print("action: ", action)
+    # print("action: ", action)
 
     cv2.imshow("RGB", transform_rgb_bgr(observations["color_sensor"]))
     cv2.imshow("depth", transform_depth(observations["depth_sensor"]))
     cv2.imshow("semantic", transform_semantic(observations["semantic_sensor"]))
     agent_state = agent.get_state()
-    sensor_state = agent_state.sensor_states['color_sensor']
+    sensor_state = agent_state.sensor_states["color_sensor"]
     print("Frame:", count)
     print("camera pose: x y z rw rx ry rz")
-    print(sensor_state.position[0],sensor_state.position[1],sensor_state.position[2], sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z)
-    
+    print(
+        sensor_state.position[0],
+        sensor_state.position[1],
+        sensor_state.position[2],
+        sensor_state.rotation.w,
+        sensor_state.rotation.x,
+        sensor_state.rotation.y,
+        sensor_state.rotation.z,
+    )
+
     count += 1
-    cv2.imwrite(data_root + f"rgb/{count}.png", transform_rgb_bgr(observations["color_sensor"]))
-    cv2.imwrite(data_root + f"depth/{count}.png", transform_depth(observations["depth_sensor"]))
-    cv2.imwrite(data_root + f"semantic/{count}.png", transform_semantic(observations["semantic_sensor"]))
-    
-    cam_extr.append([sensor_state.position[0], sensor_state.position[1], sensor_state.position[2], 
-                    sensor_state.rotation.w, sensor_state.rotation.x, sensor_state.rotation.y, sensor_state.rotation.z])
-    
+    cv2.imwrite(
+        data_root + f"rgb/{count}.png", transform_rgb_bgr(observations["color_sensor"])
+    )
+    cv2.imwrite(
+        data_root + f"depth/{count}.png", transform_depth(observations["depth_sensor"])
+    )
+    cv2.imwrite(
+        data_root + f"semantic/{count}.png",
+        transform_semantic(observations["semantic_sensor"]),
+    )
+
+    cam_extr.append(
+        [
+            sensor_state.position[0],
+            sensor_state.position[1],
+            sensor_state.position[2],
+            sensor_state.rotation.w,
+            sensor_state.rotation.x,
+            sensor_state.rotation.y,
+            sensor_state.rotation.z,
+        ]
+    )
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--floor', type=int, default=1)  
+parser.add_argument("-f", "--floor", type=int, default=1)
 args = parser.parse_args()
 
 cfg = make_simple_cfg(sim_settings)
@@ -145,10 +177,10 @@ action_names = list(cfg.agents[sim_settings["default_agent"]].action_space.keys(
 print("Discrete action space: ", action_names)
 
 
-FORWARD_KEY="w"
-LEFT_KEY="a"
-RIGHT_KEY="d"
-FINISH="f"
+FORWARD_KEY = "w"
+LEFT_KEY = "a"
+RIGHT_KEY = "d"
+FINISH = "f"
 print("#############################")
 print("use keyboard to control the agent")
 print(" w for go forward  ")
@@ -163,10 +195,12 @@ if args.floor == 1:
 elif args.floor == 2:
     data_root = "data_collection/second_floor/"
 
-if os.path.isdir(data_root): 
-    shutil.rmtree(data_root)  # WARNING: this line will delete whole directory with files
+if os.path.isdir(data_root):
+    shutil.rmtree(
+        data_root
+    )  # WARNING: this line will delete whole directory with files
 
-for sub_dir in ['rgb/', 'depth/', 'semantic/']:
+for sub_dir in ["rgb/", "depth/", "semantic/"]:
     os.makedirs(data_root + sub_dir)
 
 count = 0
@@ -194,4 +228,4 @@ while True:
         print("INVALID KEY")
         continue
 
-np.save(data_root + 'GT_pose.npy', np.asarray(cam_extr))
+np.save(data_root + "GT_pose.npy", np.asarray(cam_extr))
--- hw1/reconstruct.py
+++ hw1/reconstruct.py
@@ -4,7 +4,7 @@ import argparse
 
 
 def depth_image_to_point_cloud(rgb, depth):
-    # TODO: Get point cloud from rgb and depth image 
+    # TODO: Get point cloud from rgb and depth image
     raise NotImplementedError
     return pcd
 
@@ -15,8 +15,9 @@ def preprocess_point_cloud(pcd, voxel_size):
     return pcd_down
 
 
-def execute_global_registration(source_down, target_down, source_fpfh,
-                                target_fpfh, voxel_size):
+def execute_global_registration(
+    source_down, target_down, source_fpfh, target_fpfh, voxel_size
+):
     raise NotImplementedError
     return result
 
@@ -48,35 +49,39 @@ def reconstruct(args):
     return result_pcd, pred_cam_pos
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--floor', type=int, default=1)
-    parser.add_argument('-v', '--version', type=str, default='my_icp', help='open3d or my_icp')
-    parser.add_argument('--data_root', type=str, default='data_collection/first_floor/')
+    parser.add_argument("-f", "--floor", type=int, default=1)
+    parser.add_argument(
+        "-v", "--version", type=str, default="my_icp", help="open3d or my_icp"
+    )
+    parser.add_argument("--data_root", type=str, default="data_collection/first_floor/")
     args = parser.parse_args()
 
     if args.floor == 1:
         args.data_root = "data_collection/first_floor/"
     elif args.floor == 2:
         args.data_root = "data_collection/second_floor/"
-    
+
     # TODO: Output result point cloud and estimated camera pose
-    '''
+    """
     Hint: Follow the steps on the spec
-    '''
+    """
     result_pcd, pred_cam_pos = reconstruct()
 
     # TODO: Calculate and print L2 distance
-    '''
+    """
     Hint: Mean L2 distance = mean(norm(ground truth - estimated camera trajectory))
-    '''
-    print("Mean L2 distance: ", )
+    """
+    print(
+        "Mean L2 distance: ",
+    )
 
     # TODO: Visualize result
-    '''
+    """
     Hint: Sould visualize
     1. Reconstructed point cloud
     2. Red line: estimated camera pose
     3. Black line: ground truth camera pose
-    '''
+    """
     o3d.visualization.draw_geometries()
